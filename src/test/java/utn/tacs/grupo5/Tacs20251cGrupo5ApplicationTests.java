1217a0b66e22f0dcbc0529ead970d1d9f5c628fd
Marcos Pedaci <mpedaci@frba.utn.edu.ar>
2025-04-02 11:22:08 -0300

    first commit

---
+++ src/test/java/utn/tacs/grupo5/Tacs20251cGrupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.tacs.grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tacs20251cGrupo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
