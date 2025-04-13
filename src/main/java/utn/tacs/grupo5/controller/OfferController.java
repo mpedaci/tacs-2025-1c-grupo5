d987664f92256b7168be46c376c17043dc3931ed
moli <fran.moli222@gmail.com>
2025-04-13 05:46:03 -0300

    Base merge 2

--- src/main/java/utn/tacs/grupo5/controller/OfferController.java
+++ src/main/java/utn/tacs/grupo5/controller/OfferController.java
@@ -10,9 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.catalina.Store;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import utn.tacs.grupo5.controller.response.CustomError;
 import utn.tacs.grupo5.controller.response.ResponseGenerator;
 import utn.tacs.grupo5.dto.offer.OfferInputDto;
@@ -29,7 +27,7 @@ public class OfferController extends BaseController {
         this.offerService = offerService;
     }
 
-    @PostMapping(value = "/post/id/offers", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/post/{id}/offers", consumes = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "Create a new offer", description = "Create a new offer")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "409", content = {
@@ -42,7 +40,57 @@ public class OfferController extends BaseController {
         return ResponseGenerator.generateResponseOK("Offer saved successfully");
     }
 
-    //TODO : Implement the rest of the methods
+    @PostMapping(value = "/post/{postId}/offers/{offerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Change offer status", description = "Change offer status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomError.class))
+            }),
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<String> patch(@PathVariable String offerId, @PathVariable String postId) {
+//        offerService.save(offerDto);
+        return ResponseGenerator.generateResponseOK("Offer saved successfully");
+    }
+
+    @DeleteMapping(value = "/post/{postId}/offers/{offerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Delete an offer", description = "Delete an offer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomError.class))
+            }),
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<String> delete(@PathVariable String offerId, @PathVariable String postId) {
+//        offerService.save(offerDto);
+        return ResponseGenerator.generateResponseOK("Offer saved successfully");
+    }
+
+    @GetMapping(value = "/post/{postId}/offers/{offerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get an offer", description = "Get an offer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomError.class))
+            }),
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<String> getOffer(@PathVariable String offerId, @PathVariable String postId) {
+//        offerService.save(offerDto);
+        return ResponseGenerator.generateResponseOK("Offer saved successfully");
+    }
+
+    @GetMapping(value = "/post/{postId}/offers", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Get all offers", description = "Get all offers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomError.class))
+            }),
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
+    public ResponseEntity<String> getAllOffers( @PathVariable String postId) {
+//        offerService.save(offerDto);
+        return ResponseGenerator.generateResponseOK("Offer saved successfully");
+    }
 
 
 }
