b292eebf3c4db19a270cda598ad4a09186c02e95
moli <fran.moli222@gmail.com>
2025-05-18 14:52:29 -0300

    Fix de user repo y tests de mapper

--- backend/src/main/java/utn/tacs/grupo5/repository/impl/MongoUserRepository.java
+++ backend/src/main/java/utn/tacs/grupo5/repository/impl/MongoUserRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface MongoUserRepository extends MongoRepository<User, UUID> {
-    Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
 }
